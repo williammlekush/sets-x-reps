911dd9be8351b475e0424032280c97b329ffbd9f
mlekush <w.mlekush@gmail.com>
2026-07-27 20:19:06 -0500

    closes #8, closes #9 - feat: complete weights input form

--- src/components/content/weightsCalculator.jsx
+++ src/components/content/weightsCalculator.jsx
@@ -1,17 +1,70 @@
+import { Flex } from "@chakra-ui/react";
 import { useState } from "react";
+import FormContext from "../../contexts/formContext";
 import { NumberField } from "../shared/numberField";
 
+const WEIGHTS_IN_FORM_KEY = {
+  ORM: "in-orm",
+  RI: "in-ri",
+  REPS: "in-reps",
+};
+
 export const WeightsCalculator = () => {
-  const [oneRepMax, setOneRepMax] = useState("");
+  const [form, setForm] = useState({
+    [WEIGHTS_IN_FORM_KEY.ORM]: "",
+    [WEIGHTS_IN_FORM_KEY.RI]: "",
+    [WEIGHTS_IN_FORM_KEY.REPS]: "",
+  });
+
+  const onSubmitWeightsForm = (e) => {
+    e.preventDefault();
+  };
 
   return (
-    <form>
-      <NumberField
-        id="in-orm"
-        value={oneRepMax}
-        setValue={setOneRepMax}
-        label="One Rep Max"
-      />
-    </form>
+    <FormContext.Provider value={[form, setForm]}>
+      <form onSubmit={onSubmitWeightsForm}>
+        <Flex direction="column" alignItems="center" gapY={4}>
+          <NumberField
+            id={WEIGHTS_IN_FORM_KEY.ORM}
+            label="One Rep Max"
+            root={{
+              min: 0,
+              max: 999,
+              formatOptions: { maximumFractionDigits: 0 },
+            }}
+            input={{
+              placeholder: "###",
+              maxWidth: "3.2ch",
+            }}
+          />
+          <NumberField
+            id={WEIGHTS_IN_FORM_KEY.RI}
+            label="Relative Intensity"
+            root={{
+              min: 0,
+              max: 1,
+              formatOptions: { style: "percent" },
+            }}
+            input={{
+              placeholder: "0%",
+              maxWidth: "4.5ch",
+            }}
+          />
+          <NumberField
+            id={WEIGHTS_IN_FORM_KEY.REPS}
+            label="Reps per Set"
+            root={{
+              min: 0,
+              max: 50,
+              formatOptions: { maximumFractionDigits: 0 },
+            }}
+            input={{
+              placeholder: "##",
+              maxWidth: "2.2ch",
+            }}
+          />
+        </Flex>
+      </form>
+    </FormContext.Provider>
   );
 };
--- src/components/shared/numberField.jsx
+++ src/components/shared/numberField.jsx
@@ -1,22 +1,20 @@
 import { Field, NumberInput } from "@chakra-ui/react";
+import { useContext } from "react";
+import FormContext from "../../contexts/formContext";
 
-export const NumberField = ({ id, value, setValue, label }) => {
+export const NumberField = ({ id, label, root, input }) => {
+  const [form, setForm] = useContext(FormContext);
+
   return (
-    <Field.Root id={id} flexDirection="column" alignItems="center">
+    <Field.Root id={id}>
       <NumberInput.Root
         type="number"
-        value={value}
-        onValueChange={(e) => setValue(e.value)}
-        min={0}
-        max={999}
         variant="flushed"
+        value={form[id]}
+        onValueChange={(e) => setForm((prev) => ({ ...prev, [id]: e.value }))}
+        {...root}
       >
-        <NumberInput.Input
-          placeholder="###"
-          fontSize="8xl"
-          height="fit"
-          maxWidth="3.2ch"
-        />
+        <NumberInput.Input fontSize="8xl" height="fit" {...input} />
         <NumberInput.Control visibility="hidden" />
       </NumberInput.Root>
       <Field.Label textStyle="4xl">{label}</Field.Label>
