aadb96c191614e95ce248bfa538421ae2d125f84
mlekush <w.mlekush@gmail.com>
2026-08-02 14:04:29 -0500

    closes #36, closes #37, closes #31 - feat: add menu navigation

---
+++ src/components/content/Header/NavButton.jsx
@@ -0,0 +1,32 @@
+import { Button } from "@chakra-ui/react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+export const NavButton = ({ label, route, isOnRouteCallback, ...rest }) => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const isOnRoute = pathname === route;
+
+  return (
+    <Button
+      rounded={0}
+      variant={isOnRoute ? "surface" : "outline"}
+      flex={1}
+      colorPalette="cyan"
+      fontSize={{ base: "3xl", md: "4xl" }}
+      gap={0}
+      borderWidth="1px"
+      borderColor="cyan.400"
+      onClick={() => {
+        if (isOnRoute) {
+          isOnRouteCallback();
+          return;
+        }
+        navigate(route);
+      }}
+      {...rest}
+    >
+      {label}
+    </Button>
+  );
+};
