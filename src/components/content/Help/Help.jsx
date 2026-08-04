dd7e3f9302a168b6c734a8c5bc9d74cdf2e08d2d
mlekush <w.mlekush@gmail.com>
2026-08-04 17:08:38 -0500

    fix: make title usage conditional if help page versus help overlay

--- src/components/content/Help/Help.jsx
+++ src/components/content/Help/Help.jsx
@@ -1,10 +1,14 @@
 import { Flex } from "@chakra-ui/react";
+import { useLocation } from "react-router-dom";
 import { useTitle } from "../../../hooks/useTitle";
+import { ROUTE } from "../../../util/constants";
 import { ExerciseProtocolsSection } from "./sub/ExerciseProtocolsSection";
 import { OneRepMaxSection } from "./sub/OneRepMaxSection";
 
 export const Help = () => {
-  useTitle(`Repulator - Help`);
+  const { pathname } = useLocation();
+
+  useTitle(pathname === ROUTE.HELP ? `Repulator - Help` : document.title);
 
   return (
     <Flex direction="column" alignItems="stretch" gap={8}>
