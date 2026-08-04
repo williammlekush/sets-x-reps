475f13c3cf8db433f806f81b402e91a66316e80b
mlekush <w.mlekush@gmail.com>
2026-08-03 21:07:09 -0500

    closes #53 - fix: force dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider>
+    <Provider forcedTheme="dark">
       <Router />
     </Provider>
   </StrictMode>,
